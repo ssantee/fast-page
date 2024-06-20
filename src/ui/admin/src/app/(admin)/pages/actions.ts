62e02a150376ab07186ef294dfa11a472167c0c9
Steven Santee <ssantee@icloud.com>
2024-06-20 10:39:51 -0400

    start pages view and actions

---
+++ src/ui/admin/src/app/(admin)/pages/actions.ts
@@ -0,0 +1,20 @@
+"use server";
+
+// https://dev.fpapi.santee.cloud/page
+
+export async function createPage() {
+  // ...
+  // Call api.
+}
+
+export async function readPage() {
+  // ...
+}
+
+export async function updatePage() {
+  // ...
+}
+
+export async function deletePage() {
+  // ...
+}
---
+++ src/ui/admin/src/app/(admin)/pages/page.tsx
@@ -0,0 +1,14 @@
+export default function Analytics() {
+  // const { user, authStatus, signOut, toSignUp, toForgotPassword } =
+  //   useAuthenticator((context) => [context.user]);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <h1>Hello Analytics</h1>
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Some copy
+        </p>
+      </div>
+    </main>
+  );
+}
